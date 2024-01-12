758f3e3bfefbe82f7cb86ce25d3bb5d7130447ff
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2024-01-11 20:00:43 -0700

    Update redis.ts

--- lib/providers/Redis/redis.ts
+++ lib/providers/Redis/redis.ts
@@ -1,9 +1,12 @@
 import { Redis } from 'ioredis';
 import { promisify } from 'util';
-
+const port =
+  process.env.NODE_ENV === 'development'
+    ? '6379'
+    : '6380';
 const getRedisUrl = () => {
   if (process.env.AZURE_REDIS_URL as string) {
-    return process.env.AZURE_REDIS_URL! as string;
+    return process.env.AZURE_REDIS_URL! + port as string;
   }
   throw new Error('REDIS url is not defined');
 };
