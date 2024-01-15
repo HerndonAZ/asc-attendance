75e27b1235370bf40305e7b795ac387b36c9e8cb
Twinny Twin <154267258+HerndonAZ@users.noreply.github.com>
2024-01-15 08:01:07 -0700

    create auth context

---
+++ app/auth/client.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { useQuery } from "@tanstack/react-query";
+import React, { createContext, useCallback, useContext, useMemo } from "react";
+import { AuthState, useAuthStore } from "./store";
+import { auth } from ".";
+import { useRouter } from "next/navigation";
+
+// const refresh = () => {
+//   window.location.reload();
+// };
+
+export const AuthContext = createContext<AuthState | any>(useAuthStore.getState());
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const {
+    setUserRole,
+    userRole,
+    isLoading
+  } = useAuthStore();
+  const setProfileState = (profile: any) => useAuthStore.setState({ profile })
+
+  const setProfile = useCallback((profile: any) => {
+    setProfileState(profile)
+  },[setProfileState])
+
+  const router = useRouter();
+
+   const {data:session} = useQuery({
+     queryKey: ['session'],
+     queryFn: () => auth()
+  
+  })
+
+
+  const value = useMemo(
+    () => ({
+      session: (session) ,
+      isLoading,
+      userRole,
+      setUserRole,
+    }),
+    [
+      session,
+      userRole,
+      setUserRole,
+      isLoading,
+  
+    ],
+  );
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuthProvider = () => {
+  return useContext(AuthContext);
+};
---
+++ app/auth/store.ts
@@ -0,0 +1,25 @@
+import { toast } from "react-toastify";
+import { create } from "zustand";
+
+export enum UserRoleTypes {
+  user = "user",
+  admin = "admin",
+  editor = "editor",
+}
+export interface AuthState {
+  user: any;
+  profile: any;
+  isLoading: boolean;
+  userRole: any;
+  setUserRole: (userRole: string) => void;
+  unsubscribeAuthListener: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  userRole: null,
+  user: null,
+  profile: null,
+  isLoading: false,
+  setUserRole: (userRole: any) => set({ userRole }),
+  unsubscribeAuthListener: () => {},
+}));
