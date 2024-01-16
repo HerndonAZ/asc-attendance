6c5efce3dbcd164290c54dc694a262e647a93810
Twinny Twin <154267258+HerndonAZ@users.noreply.github.com>
2024-01-16 13:26:49 -0700

    update pricing type labels

--- ui/Components/RealTime/actions.tsx
+++ ui/Components/RealTime/actions.tsx
@@ -16,6 +16,7 @@ export const getTableData = ({data, useMerged} : {data: any, useMerged: boolean}
           existingItem.attendance += current.attendance;
           existingItem.revenue += current.revenue;
         } else {
+            
           // If the item doesn't exist, add it to the accumulator
           acc.push({ ...current });
         }
@@ -35,26 +36,51 @@ export const getPriceType = (priceType: string | number) => {
 const member: any[] = [7,21,956]
 const group: any[] = [16]
 const nonMember: any[] = [2,1,3,5,6]
-const groupon: any = [350]
+const groupon: any = 350
 
 
 
     if (member.includes(priceType)){
-        return "Member"
+        return <PriceTypeBadge priceType="Member"/>
     }
 
     if (nonMember.includes(priceType)){
-        return "Non-member"
+      return <PriceTypeBadge priceType="Non-Member"/>
     }
 
     if (group.includes(priceType)){
-        return "Group"
+      return <PriceTypeBadge priceType="Group"/>
     }   
 
     if (priceType === groupon){
-        return "Groupon"
+      return <PriceTypeBadge priceType="Groupon"/>
     }
 
     return null
 
+}
+
+const PriceTypeBadge = ({priceType} : {priceType: string}) => {
+
+  if (priceType === "Member")
+return (
+  <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{priceType}</span>
+)
+
+if (priceType === "Non-Member")
+return (
+  <span className="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{priceType}</span>
+)
+
+
+
+if (priceType === "Group")
+return (
+  <span className="bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{priceType}</span>
+)
+
+if (priceType === "Groupon")
+return (
+  <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{priceType}</span>
+)
 }
