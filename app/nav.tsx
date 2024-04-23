3869509c4749b5d9e600b418e0427a01e360b81c
Twinny Twin <154267258+HerndonAZ@users.noreply.github.com>
2024-04-23 09:02:55 -0700

    Update nav.tsx

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,11 +1,8 @@
-import { getToday } from '@/lib/db';
 import { auth } from './auth';
 import Navbar from './navbar';
 
 export default async function Nav() {
 
-  const date = getToday()
-  console.log(date, 'date')
   const session = await auth();
   return <Navbar user={session?.user} />;
 }
