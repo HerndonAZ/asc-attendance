c9b10d92e6f956d7f6bb5977a74a7a5df28370fd
Twinny Twin <herndonr@azscience.org>
2024-06-13 09:13:17 -0700

    update script and dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import SiteInBetaNotice from '@/ui/Components/Notices/SiteInBetaNotice';
 import { Analytics } from '@vercel/analytics/react';
 import Providers from 'lib/providers';
+import Script from 'next/script';
 import { Suspense } from 'react';
 import Nav from './nav';
 import Toast from './toast';
@@ -20,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <script defer src="https://unpkg.com/@tinybirdco/flock.js" data-host="https://api.tinybird.co" data-token="p.eyJ1IjogImRhZmVkZjE2LTc1ZTYtNGMwOS1hZWFmLTRlM2Q2MDEwYjA5OSIsICJpZCI6ICI4OTEwMjJhZi0xOGU4LTQ4N2UtYjdiNy02M2YxMjMxZjk1MzEiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.hmpG5z0MvTXwJl8WSHaUcyWkrLn3mb5pqLTlwGRD9C4"></script>
+      <Script defer src="https://unpkg.com/@tinybirdco/flock.js" data-host="https://api.tinybird.co" data-token="p.eyJ1IjogImRhZmVkZjE2LTc1ZTYtNGMwOS1hZWFmLTRlM2Q2MDEwYjA5OSIsICJpZCI6ICI4OTEwMjJhZi0xOGU4LTQ4N2UtYjdiNy02M2YxMjMxZjk1MzEiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.hmpG5z0MvTXwJl8WSHaUcyWkrLn3mb5pqLTlwGRD9C4"></Script>
       <body className="h-full bg-gray-50 dark:bg-gray-900 ">
         <Providers>
           <Suspense>
